2b877d3b23d98b863acd5042cb3cceb94e9beb39
zin9349 <zin9349@outlook.com>
2021-05-20 15:43:14 +0900

    update:allSelect実装

--- lib/dbInterface.dart
+++ lib/dbInterface.dart
@@ -10,13 +10,20 @@ class DbInterface {
   // idを指定して取得
   static Future<List> select(String _tableName, var database, int id) async {
     final Database db = await database;
-    final List<Map<String, dynamic>> maps =
-        await db.query(
-          '$_tableName', 
-          where: 'id = ?',
-          orderBy: 'id asc',
-          whereArgs: [id]
-        );
+    final List<Map<String, dynamic>> maps = await db.query('$_tableName',
+        where: 'id = ?', orderBy: 'id asc', whereArgs: [id]);
+    return maps;
+  }
+
+  /*
+  @param _tableName テーブル名
+  @param database   DB 
+  */
+  //全件取得
+  static Future<List> allSelect(String _tableName, var database) async {
+    final Database db = await database;
+    final List<Map<String, dynamic>> maps = await db.query(_tableName);
+    // List.generate(maps.length, (i) => {print(maps[i])});
     return maps;
   }
 
@@ -26,7 +33,8 @@ class DbInterface {
   @param values     テーブルの型を持った更新内容   
   */
   // 追加
-  static Future<void> insert(String _tableName, var database, var values) async {
+  static Future<void> insert(
+      String _tableName, var database, var values) async {
     final Database db = await database;
     await db.insert(
       '$_tableName',
@@ -41,7 +49,8 @@ class DbInterface {
   @param values     テーブルの型を持った更新内容   
   */
   // 更新
-  static Future<void> update(String _tableName, var database, var values) async {
+  static Future<void> update(
+      String _tableName, var database, var values) async {
     // Get a reference to the database.
     final db = await database;
     await db.update(
@@ -67,5 +76,4 @@ class DbInterface {
       whereArgs: [id],
     );
   }
-
 }
