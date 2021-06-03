b96133c5b249fba3893db4ea91a090f23543a3e4
keykimu <kimuyuu0309@gmail.com>
2021-06-03 16:47:33 +0900

    update:テストコードの作成

--- lib/Util/toDateOrTime.dart
+++ lib/Util/toDateOrTime.dart
@@ -3,7 +3,7 @@ import 'package:stamp_app/Util/enumDateType.dart';
 
 String toDateOrTime(DateTime datetime, String dateType) {
   if (dateType == enumDateType.date.toString()) {
-    return DateFormat('MM/dd/yyyy').format(datetime);
+    return DateFormat('yyyy/MM/dd').format(datetime);
   } else if (dateType == enumDateType.time.toString()) {
     return DateFormat('HH:mm:ss').format(datetime);
   }
