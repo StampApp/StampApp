46f9a4ae74531c4f2cd2570c3371b8f13f219846
Bridgehead <ase18410220@wiz.ac.jp>
2021-06-08 09:44:56 +0900

    update:引数追加

--- test/checkIsMaxStampsTest.dart
+++ test/checkIsMaxStampsTest.dart
@@ -3,19 +3,21 @@ import 'package:stamp_app/checkIsMaxStamps.dart';
 
 void main() {
   group('checkIsMaxStamps test', () {
-    test("checkIsMaxStamps true test", () {
+    //第一引数(スタンプ数) + 1が第二引数(スタンプ上限数)以上だったらtrue、それ以外はfalse
+    test("checkIsMaxStamps(2, 3) true test", () {
       expect(checkIsMaxStamps(2, 3), true);
     });
 
-    test("checkIsMaxStamps true test", () {
+    test("checkIsMaxStamps(3, 3) true test", () {
       expect(checkIsMaxStamps(3, 3), true);
     });
 
-    test("checkIsMaxStamps false test", () {
+    //第二引数が0だったら必ずfalse
+    test("checkIsMaxStamps(3, 0) false test", () {
       expect(checkIsMaxStamps(3, 0), false);
     });
 
-    test("checkIsMaxStamps false test", () {
+    test("checkIsMaxStamps(1, 3) false test", () {
       expect(checkIsMaxStamps(1, 3), false);
     });
   });
