033ec191e1c4b6ec3aa539c87078f2f9ad88620d
Bridgehead <ase18410220@wiz.ac.jp>
2021-06-09 12:24:00 +0900

    update:エラーハンドリングとテストの追加

--- lib/Widget/HexColor.dart
+++ lib/Widget/HexColor.dart
@@ -1,13 +1,28 @@
 //色をカラーコードで指定するためのクラス
 import 'dart:ui';
 
+class HexException implements Exception {
+  String cause;
+  HexException(this.cause);
+}
+
 class HexColor extends Color {
   static int _getColorFromHex(String hexColor) {
     hexColor = hexColor.toUpperCase().replaceAll('#', '');
-    if (hexColor.length == 6) {
-      hexColor = 'FF' + hexColor;
+    try {
+      if (hexColor.length == 6) {
+        hexColor = 'FF' + hexColor;
+        return int.parse(hexColor, radix: 16);
+      } else {
+        throw new HexException('The characters are not 6 characters.');
+      }
+    } on HexException catch (e) {
+      print('HexException catch ${e.cause}');
+      rethrow;
+    } catch (e) {
+      print('catch:$e');
+      rethrow;
     }
-    return int.parse(hexColor, radix: 16);
   }
 
   HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
