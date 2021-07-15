347c56eb3fae97f842976219d972e9e594fedc04
RimlTempest <riml.slime@gmail.com>
2021-07-16 03:03:12 +0900

    update: DB情報を定数に一元管理

--- lib/Constants/setting.dart
+++ lib/Constants/setting.dart
@@ -1,7 +1,9 @@
 class Setting {
   static const String VERSION = '0.2.5';
+  static const String DB_PATH = 'stamp_database.db';
   static const String APP_COLOR = '00C2FF';
   static const String APP_LOGO = 'assets/images/other/logo_Contrast.png';
   static const String NONE_IMG = 'assets/images/stamp/none.png';
   static const String STAMP_FLOWER = 'assets/images/stamp/flower-4.png';
+  static const Map<String, String> DB_TABLES = { "STAMP": 'Stamp', "LOGS": 'StampLogs'};
 }
