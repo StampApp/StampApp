9ea2b590c9c9d833a9e30f632a778491608b0d6d
keykimu <kimuyuu0309@gmail.com>
2021-07-21 12:11:16 +0900

    fix:使い方画面の画像を画像と説明文に分離

--- lib/View/instructions.dart
+++ lib/View/instructions.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:page_view_indicators/circle_page_indicator.dart';
 import 'package:stamp_app/Constants/setting.dart';
 import 'package:stamp_app/Widget/HexColor.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class InstructionsPage extends StatefulWidget {
   InstructionsPage({Key key, this.title}) : super(key: key);
@@ -43,12 +44,7 @@ class _IntroductionState extends State<Introduction> {
     "assets/images/instructions/setting.jpg",
     "assets/images/instructions/history.jpg",
   ];
-  // 表示するテキスト
-  List<String> _textList = [
-    "ホーム画面の説明",
-    "設定画面の説明",
-    "利用履歴の説明",
-  ];
+
   @override
   void initState() {
     super.initState();
@@ -90,6 +86,41 @@ class _IntroductionState extends State<Introduction> {
   Widget build(BuildContext context) {
     //デバイスのサイズを変数deviceHeightへ
     final double deviceHeight = MediaQuery.of(context).size.height;
+    //説明文
+    List<String> _explanatoryNote = [
+      "①" +
+          AppLocalizations.of(context).goToSetting +
+          "\n②" +
+          AppLocalizations.of(context).showHaveStamp +
+          "\n③" +
+          AppLocalizations.of(context).goToScanning,
+      "①" +
+          AppLocalizations.of(context).goToPrevious +
+          "\n②" +
+          AppLocalizations.of(context).goToHistory +
+          "\n③" +
+          AppLocalizations.of(context).currentScreen +
+          "\n④" +
+          AppLocalizations.of(context).goToUse +
+          "\n⑤" +
+          AppLocalizations.of(context).jumpPrivacyPolicy +
+          "\n⑥" +
+          AppLocalizations.of(context).version +
+          "\n⑦" +
+          AppLocalizations.of(context).useAccumulatedStamps,
+      "①" +
+          AppLocalizations.of(context).goToPrevious +
+          "\n②" +
+          AppLocalizations.of(context).sortTimeOfUse +
+          "\n③" +
+          AppLocalizations.of(context).displaysUsageHistory,
+    ];
+    // 表示するテキスト
+    List<String> _textList = [
+      AppLocalizations.of(context).descriptionHome,
+      AppLocalizations.of(context).explanationSetting,
+      AppLocalizations.of(context).explanationUsageHistory,
+    ];
     return SafeArea(
       child: Column(
         children: <Widget>[
@@ -107,6 +138,25 @@ class _IntroductionState extends State<Introduction> {
                   active,
                 );
               },
+            ),
+          ),
+          //説明本文
+          Container(
+            height: deviceHeight * 0.3,
+            padding: EdgeInsets.all(7.0),
+            child: Container(
+              width: deviceHeight * 0.4, //double.infinity,
+              padding: EdgeInsets.all(7.0),
+              child: Center(
+                child: Text(
+                  _explanatoryNote[_currentPageNotifier.value],
+                  style: TextStyle(
+                    //横長のデバイス上でTextがつぶれないよう
+                    fontSize: deviceHeight * 0.022,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ),
           ),
           // ページインジケータ
