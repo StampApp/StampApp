1b368c5b29416d053508432bd8fce017ae410a8a
yoshida874 <ud8mnzdd@gmail.com>
2021-07-28 11:39:36 +0900

    clean: 読み込み日時を太字に変更

--- lib/Widget/stampDialog.dart
+++ lib/Widget/stampDialog.dart
@@ -21,7 +21,10 @@ void stampDialog(BuildContext context, Stamp stamp) {
               ),
               ListTile(
                 // TODO: i18n対応
-                title: Text('スタンプ読み込み日時'),
+                title: Text(
+                  'スタンプ読み込み日時',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 subtitle: Text(
                   dateFormat(stamp.createdAt),
                   style: TextStyle(color: Colors.black, fontSize: 15, height: 2)
