94c44aafcefd6630ef7ce51f93e0665ea164854f
RimlTempest <riml.slime@gmail.com>
2021-07-30 10:02:45 +0900

    update: dateFormatを共通化

--- lib/Util/toDateOrTime.dart
+++ lib/Util/toDateOrTime.dart
@@ -1,4 +1,5 @@
 import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:stamp_app/Util/Enums/enumDateType.dart';
 
 // datetime からstringへの変換
@@ -26,4 +27,12 @@ DateTime formatStringToDateTime(String strDateTime, EnumDateType dateType) {
       DateFormat dateFormatter = DateFormat('yyyy-MM-dd');
       return dateFormatter.parseStrict(strDateTime);
   }
+}
+
+String dateFormat(createdAt) {
+  //createdAtを「yyyy年MM月DD日(E) hh:mm:ss」という表記に変更する関数
+  initializeDateFormatting('ja');
+  String data = DateFormat.yMMMEd('ja').format(createdAt).toString() + "　";
+  data += DateFormat.jms('ja').format(createdAt).toString();
+  return data;
 }
