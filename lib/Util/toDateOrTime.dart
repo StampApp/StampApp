96f6c8f3439cff6250dd8800fbd1a2b4b62d36d2
kyosuke24 <58577250+kyosuke24@users.noreply.github.com>
2021-08-24 11:32:09 +0900

    関数のDocを追加

--- lib/Util/toDateOrTime.dart
+++ lib/Util/toDateOrTime.dart
@@ -2,7 +2,15 @@ import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:stamp_app/Util/Enums/enumDateType.dart';
 
-// datetime からstringへの変換
+///時刻型から文字列型へ変換
+///
+///第二引数が[date]の場合は「yyyy-MM-dd」へ変更
+///第二引数が[time]の場合は「HH:mm:ss」へ変更
+///
+///```
+///formatDateTimeToString(DateTime(2021, 6, 3, 16, 25, 10, 10, 10), EnumDateType.date)
+///formatDateTimeToString(DateTime(2021, 6, 3, 16, 25, 10, 10, 10), EnumDateType.time)
+///```
 String formatDateTimeToString(DateTime datetime, EnumDateType dateType) {
   switch (dateType) {
     case EnumDateType.date:
@@ -13,8 +21,14 @@ String formatDateTimeToString(DateTime datetime, EnumDateType dateType) {
       return DateFormat('yyyy-MM-dd').format(datetime);
   }
 }
-
-// stringからdatetimeへの変換
+///文字列型から時刻型に変更
+///
+///第一引数[strDateTime]と第二引数[dateType]を受け取り文字列型に変更して返す
+///
+///```
+///formatStringToDateTime(maps[i]['stamp_date'], EnumDateType.date)
+///formatStringToDateTime(maps[i]['stamp_time'], EnumDateType.time)
+///```
 DateTime formatStringToDateTime(String strDateTime, EnumDateType dateType) {
   switch (dateType) {
     case EnumDateType.date:
@@ -28,9 +42,14 @@ DateTime formatStringToDateTime(String strDateTime, EnumDateType dateType) {
       return dateFormatter.parseStrict(strDateTime);
   }
 }
-
+///年月日表記に変更
+///
+///引数[createdAt]を「yyyy年MM月DD日(E) hh:mm:ss」表記に変更
+///
+///```
+///dateFormat(stamp.createdAt)
+///```
 String dateFormat(createdAt) {
-  //createdAtを「yyyy年MM月DD日(E) hh:mm:ss」という表記に変更する関数
   initializeDateFormatting('ja');
   String data = DateFormat.yMMMEd('ja').format(createdAt).toString() + "　";
   data += DateFormat.jms('ja').format(createdAt).toString();
