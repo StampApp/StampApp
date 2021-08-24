69453390d9429741343b867df65ab1f0aac9a354
yamakou0808 <yamakou311@gmail.com>
2021-08-24 16:04:36 +0900

    Validation.dartのdoc修正（空白等）

--- lib/Util/validation.dart
+++ lib/Util/validation.dart
@@ -1,11 +1,11 @@
-// 読み込んだQRを検証するクラス
 import 'package:intl/intl.dart';
 import 'package:stamp_app/Util/Enums/enumCheckString.dart';
-///QRコードが現時刻より昔だった場合trueを返す
+
+/// QRコードが現時刻より昔だった場合trueを返す
 ///
-///[dateCheck],引数QRコードの時間　戻り値boolean
-///[verificationDate]DateTime型　受け取った時刻をyyyy/MM/dd HH:mm:ss形式に
-///[now]DateTime型　現時刻
+/// [dateCheck],引数QRコードの時間　戻り値boolean
+/// [verificationDate]DateTime型　受け取った時刻をyyyy/MM/dd HH:mm:ss形式に
+/// [now]DateTime型　現時刻
 ///
 class Validation {
   static final String stampCheckString = CheckString.ok.checkStringValue!;
@@ -18,15 +18,16 @@ class Validation {
     return true;
   }
 
-///正規表現チェック
+
+/// 正規表現チェック
 ///
-///[strCheck] 引数[content]string 戻り値boolean
+/// [strCheck] 引数[content]string 戻り値boolean
 ///
 ///```
-///[content]1(正規表現)
-///         2([stampCheckString]の内容が含まれない)
-///         3(httpの頭文字)
-///[content]が上記の場合false
+/// [content]1(正規表現)
+///          2([stampCheckString]の内容が含まれない)
+///          3(httpの頭文字)
+/// [content]が上記の場合false
 ///```
   static bool strCheck(String content) {
     // 記号・半角を含まない
@@ -35,11 +36,13 @@ class Validation {
     if (content.contains('http')) return false;
     return true;
   }
-///ファイルがあるか確認
+
+
+/// ファイルがあるか確認
 ///
-///[pathCheck]引数[path][imagePaths] 戻り値boolean
-///[path]String型　
-///[imagePaths]string型のリスト
+/// [pathCheck]引数[path][imagePaths] 戻り値boolean
+/// [path]String型　
+/// [imagePaths]string型のリスト
 ///
   static bool pathCheck(String path, List<String> imagePaths) {
     try {
