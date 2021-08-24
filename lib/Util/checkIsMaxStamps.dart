487ee6c729cdbf41c8ae6e0c9f78f032ebb9454e
kyosuke24 <58577250+kyosuke24@users.noreply.github.com>
2021-08-24 11:46:11 +0900

    ブランチの切り方を間違えてしまったので一時訂正

--- lib/Util/checkIsMaxStamps.dart
+++ lib/Util/checkIsMaxStamps.dart
@@ -1,10 +1,3 @@
-///スタンプの比較
-///
-///第一引数[successStampLen]+1 が第二引数[maxStamp] 以上の場合trueを返す
-///
-///```
-///checkIsMaxStamps(2,3) -> ture
-///```
 bool checkIsMaxStamps(int successStampLen, int maxStamp) {
   //上限無しの場合0を指定
   if (maxStamp == 0) return false;
